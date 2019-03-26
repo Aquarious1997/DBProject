bcac67726c319c20f63b6136a60ddfc30a1c64c2
Sheharyar <38451744+Aquarious1997@users.noreply.github.com>
2019-03-27 01:12:45 +0500

    By Sheharyar
    Attendance Scheme

---
+++ LabProject/AttendenceF.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LabProject
+{
+    public partial class AttendenceF : Form
+    {
+        public AttendenceF()
+        {
+            InitializeComponent();
+        }
+
+        string rep;
+        public string constr = "Data Source=DESKTOP-5DTSEQD;Initial Catalog=ProjectB;Integrated Security=True ";
+        private void AttendenceF_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            string query = "Select Id,RegistrationNumber from Student where status="+5+"";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader doit;
+            doit = cmd.ExecuteReader();
+            DataTable tb = new DataTable();
+            tb.Columns.Add("RegistrationNumber", typeof(string));
+            tb.Columns.Add("Id", typeof(string));
+            tb.Load(doit);
+            comboBox1.ValueMember = "Id";
+            comboBox1.DisplayMember = "RegistrationNumber";
+            comboBox1.DataSource = tb;
+            comboBox1.Text = "Choose Registration# From Here";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(constr);
+            conn.Open();
+            if (checkBox1.Checked == true)
+            {
+                rep = "Present";
+            }
+            else if (checkBox2.Checked == true)
+            {
+                rep = "Absent";
+            }
+            else if (checkBox3.Checked == true)
+            {
+                rep = "Leave";
+
+            }
+            else
+            {
+                rep = "Late";
+            }
+
+            if (conn.State == ConnectionState.Open)
+            {
+                string xquery = "Select Id from Student where RegistrationNumber='" + comboBox1.Text + "'";
+                SqlCommand cmd1 = new SqlCommand(xquery, conn);
+                int maxId = int.Parse(cmd1.ExecuteScalar().ToString());
+
+                string query1 = "Select LookupId from Lookup where Name='" + rep + "'";
+                SqlCommand cmd2 = new SqlCommand(query1, conn);
+                var dt = cmd2.ExecuteReader();
+                dt.Read();
+                int stateId = (int)dt.GetValue(0);
+                dt.Close();
+                string maxquery = "select Id  from ClassAttendance";
+                SqlCommand cmd3 = new SqlCommand(maxquery, conn);
+                int maxId2 = int.Parse(cmd3.ExecuteScalar().ToString());
+                string query = "INSERT INTO StudentAttendance(AttendanceId,StudentId,AttendanceStatus) values('" + maxId2 + "','" + maxId + "' ," + stateId + ")";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Attendance Marked");
+                Form1 frm = new Form1();
+                frm.Show();
+                this.Hide();
+            }
+
+
+
+
+
+
+            else
+            {
+                MessageBox.Show("Some sort of error");
+            }
+        }
+    }
+}
---
+++ LabProject/ClassAtt.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace LabProject
+{
+    partial class ClassAtt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Date";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(67, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 25);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Date Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(67, 182);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(68, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 16);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Class Attendance Date";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(371, 51);
+            this.panel1.TabIndex = 19;
+            // 
+            // ClassAtt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(373, 427);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Name = "ClassAtt";
+            this.Text = "ClassAtt";
+            this.Load += new System.EventHandler(this.ClassAtt_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
